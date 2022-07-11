6db4444b81bfc3f63eae80c6d91dc530b0d1b253
DaoPVT <dao.phan@lussoenterprise.com>
2022-07-11 15:35:25 +0700

    Header P1. Add logo, search bar

--- src/components/Layout/components/Header/index.tsx
+++ src/components/Layout/components/Header/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import styles from './Header.module.scss';
 import classNames from 'classnames/bind';
+import images from '@/assets/images';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 // cx giúp viết className dễhơn.
 // VD: post-item thì không thể viết styles.post-item (Hoặc viết styles['post-item']) => Xấu
@@ -10,7 +13,23 @@ const cx = classNames.bind(styles);
 function Header() {
   return (
     <header className={cx('wrapper')}>
-      <div className={cx('container')}></div>
+      <div className={cx('container')}>
+        <img src={images.logo} alt="tiktok" />
+
+        <div className={cx('search')}>
+          <input type="text" placeholder="Search accounts and videos" spellCheck={false} />
+          <button className={cx('clear')}>
+            <FontAwesomeIcon icon={faCircleXmark} />
+          </button>
+          <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+
+          <button className={cx('search__btn')}>
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+          </button>
+        </div>
+
+        <div className={cx('action')}></div>
+      </div>
     </header>
   );
 }
